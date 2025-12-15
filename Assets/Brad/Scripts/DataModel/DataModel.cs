fb8da16875dcfc0b68cda5d875d0fa0fc746e7c3
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-12-15 12:52:30 +0100

    Runtime-safe List manipulation methods in the DataModel

--- Assets/Brad/Scripts/DataModel/DataModel.cs
+++ Assets/Brad/Scripts/DataModel/DataModel.cs
@@ -76,6 +76,31 @@ public class ProgressionData : IRuntimeData
 
     // TODO all the progression data fields
 
+    // list fields
+    private List<string> _defeatedEnemies;
+    public void AddDefeatedEnemy(string enemyID)
+    {
+        if (_defeatedEnemies.Contains(enemyID)) return;
+        _defeatedEnemies.Add(enemyID);
+        DataTools.HandleOnDataChanged(this);
+    }
+    public void RemoveDefeatedEnemy(string enemyID)
+    {
+        if (!_defeatedEnemies.Contains(enemyID)) return;
+        _defeatedEnemies.Remove(enemyID);
+        DataTools.HandleOnDataChanged(this);
+    }
+    public void ResetDefeatedEnemies()
+    {
+        _defeatedEnemies.Clear();
+        _defeatedEnemies = new List<string>();
+    }
+    public List<string> GetDefeatedEnemies()
+    {
+        return new List<string>(_defeatedEnemies);
+    }
+
+    // atomic fields
     private bool _talkedToBob;
     public bool TalkedToBob
     {
@@ -107,12 +132,14 @@ public class ProgressionData : IRuntimeData
         IsSandbox = false;
         TalkedToBob = false;
         TalkedToAlice = false;
+        _defeatedEnemies = new List<string>();
     }
     public ProgressionData(bool isSandBox)
     {
         IsSandbox = isSandBox;
         TalkedToBob = false;
         TalkedToAlice = false;
+        _defeatedEnemies = new List<string>();
     }
     public bool GetIsSandbox() { return IsSandbox; }
 }
