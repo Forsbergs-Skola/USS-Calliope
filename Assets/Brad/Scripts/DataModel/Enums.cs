3dba2c32b59d7d29de0c380a5f5495bea61831b8
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-12-15 15:56:46 +0100

    Added player XP
    -Also moved PlayerData, InventoryData, ProgressionData into dedicated scripts for each one.
    - (See Assets/Brad/Scripts/DataModel)

---
+++ Assets/Brad/Scripts/DataModel/Enums.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public enum EnumPlayerStatusEffect
+{
+    BLEEDING,
+    POISON,
+    IN_STEALTH
+    // add/remove more as needed
+}
+
+public enum EnumAttackType
+{
+    MELEE,
+    RANGED
+}
---
+++ Assets/Brad/Scripts/DataModel/SaveService.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+
+public interface IRuntimeData
+{
+    public bool GetIsSandbox();
+}
+
+[System.Serializable]
+// A utility class for serializing lists to JSON
+public class StringListWrapper
+{
+    public List<string> strings;
+}
+[System.Serializable] 
+public class SaveData
+// JSON friendly data store
+{
+    // TODO
+}
+
+
+[System.Serializable]
+public class GameData
+{
+    public PlayerData playerData;
+    public InventoryData inventoryData;
+    public ProgressionData progressionData;
+
+    public GameData() { }
+    public GameData(PlayerData _playerData, InventoryData _inventoryData, ProgressionData _progressionData)
+    {
+        playerData = _playerData;
+        inventoryData = _inventoryData;
+        progressionData = _progressionData;
+    }
+}
+
+public static class DataTools
+{
+    public static void HandleOnDataChanged(IRuntimeData data)
+    {
+        if (data.GetIsSandbox()) return;
+        if (EventRelay.Instance == null) return;
+        EventRelay.Instance.GameEvents.DataUpdatedEvent.TriggerEvent();
+    }
+
+    public static StringListWrapper GetWrapperizedStringList(List<string> inList)
+    {
+        StringListWrapper wrappedStrings = new StringListWrapper();
+        wrappedStrings.strings = new List<string>(inList);
+        return wrappedStrings;
+    }
+}
+
+public static class SaveService
+{
+    private static string saveFilePath => Path.Combine(Application.persistentDataPath, "save.json");
+    
+    /////////
+    // API //
+    ///////// 
+    
+    public static bool SaveExists()
+    {
+        return File.Exists(saveFilePath);
+    }
+    public static void Save(PlayerData playerData, InventoryData inventoryData, ProgressionData progressionData)
+    {
+        ClearSave();
+        SaveData newSD = GameDataToSaveData(playerData, inventoryData, progressionData);
+        // TODO -- write newSD to disk
+    }
+    public static void Load()
+    {
+        SaveData savedData = new SaveData();
+
+        // TODO -- read saved JSON into savedData
+        
+        GameData gameData = new GameData();
+        PlayerData _playerData = GetPlayerDataFromSaveData(savedData);
+        InventoryData _inventoryData = GetInventoryDataFromSaveData(savedData);
+        ProgressionData _progressionData = GetProgressionDataFromSaveData(savedData);
+        gameData.playerData = _playerData;
+        gameData.inventoryData = _inventoryData;
+        gameData.progressionData = _progressionData;
+        EventRelay.Instance.GameEvents.SavedGameLoadedEvent.TriggerEvent(gameData);
+    }
+
+    //////////////////////
+    // Conversion Tools //
+    //////////////////////
+    
+    private static SaveData GameDataToSaveData(PlayerData playerData, InventoryData inventoryData, ProgressionData progressionData)
+    {
+        SaveData outData = new SaveData();
+        // TODO...
+        return outData;
+    }
+    public static void ClearSave()
+    {
+        if (File.Exists(saveFilePath)) { File.Delete(saveFilePath); }
+    }
+    private static PlayerData GetPlayerDataFromSaveData(SaveData saveData)
+    {
+        PlayerData playerData = new PlayerData();
+        // TODO...
+        return playerData;
+    }
+    private static InventoryData GetInventoryDataFromSaveData(SaveData saveData)
+    {
+        InventoryData inventoryData = new InventoryData();
+        // TODO...
+        return inventoryData;
+    }
+    private static ProgressionData GetProgressionDataFromSaveData(SaveData saveData)
+    {
+        ProgressionData progressionData = new ProgressionData();
+        // TODO...
+        return progressionData;
+    }
+}
