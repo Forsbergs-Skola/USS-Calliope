aa2a66358856a462013bba502a8318cbd8d35552
Euan Scott <euan.scott@student.forsbergsskola.se>
2025-12-17 16:54:32 +0100

    Vision Fix
    USed a hashset to track enemies because sometimes they wouldn't disappear

--- Assets/Camera/PlayerVisionLogic.cs
+++ Assets/Camera/PlayerVisionLogic.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerVisionLogic : MonoBehaviour
@@ -8,46 +9,67 @@ public class PlayerVisionLogic : MonoBehaviour
     public LayerMask obstacleLayer;
     public float eyeHeight = 1.5f;
     
+    private HashSet<Collider> trackedEnemies = new HashSet<Collider>(); //Lieke a list but doesn't allow duplicates and have fast lookup
     
     void Update()
     {
-        Collider[] enemies = Physics.OverlapSphere(transform.position, viewDistance, enemyLayer);
+        Collider[] detectedEnemies = Physics.OverlapSphere(transform.position, viewDistance, enemyLayer);
 
-        foreach (Collider enemy in enemies)
+        foreach (Collider enemy in trackedEnemies)
         {
-            Renderer enemyRenderer = enemy.GetComponent<Renderer>();
-            SneakyEnemy sneakyEnemy = enemy.GetComponent<SneakyEnemy>();
-            
-            bool isVisible = false;
+            SetEnemyVisibility(enemy, false);
+        }
 
-            Vector3 directionToEnemy = enemy.transform.position - transform.position;
-            directionToEnemy.y = 0f;
+        trackedEnemies.Clear();
 
-            Vector3 forward = transform.forward;
-            forward.y = 0f;
+        foreach (Collider enemy in detectedEnemies)
+        {
+            bool isVisible = CheckVisibility(enemy);
 
-            float angleToEnemy = Vector3.Angle(forward, directionToEnemy);
-            
-            if (angleToEnemy <= viewAngle * 0.5f) //Check if the enemy is in FOV
-            {
-                Vector3 eyePosition = transform.position + Vector3.up * eyeHeight;
-                Vector3 enemyTarget = enemy.bounds.center;
+            SetEnemyVisibility(enemy, isVisible);
 
-                Vector3 raycastDirection = enemyTarget - eyePosition;
-                float raycastDistance = raycastDirection.magnitude;
-                
-                if (!Physics.Raycast(eyePosition, raycastDirection.normalized, raycastDistance, obstacleLayer)) //If no wall blocks enemy
-                {
-                    isVisible = true;
-                    Debug.DrawLine(eyePosition, enemyTarget, Color.green);
-                }
-            }
-
-            if (enemyRenderer != null)
-                enemyRenderer.enabled = isVisible;
-            
-            if (sneakyEnemy != null)
-                sneakyEnemy.SetSeen(isVisible);
+            trackedEnemies.Add(enemy);
         }
     }
+    
+    bool CheckVisibility(Collider enemy)
+    {
+        Vector3 directionToEnemy = enemy.transform.position - transform.position;
+        directionToEnemy.y = 0f;
+
+        Vector3 forward = transform.forward;
+        forward.y = 0f;
+
+        float angleToEnemy = Vector3.Angle(forward, directionToEnemy);
+
+        if (angleToEnemy > viewAngle * 0.5f)
+            return false;
+
+        Vector3 eyePosition = transform.position + Vector3.up * eyeHeight;
+        Vector3 enemyTarget = enemy.bounds.center;
+
+        Vector3 raycastDirection = enemyTarget - eyePosition;
+        float raycastDistance = raycastDirection.magnitude;
+
+        if (Physics.Raycast(eyePosition, raycastDirection.normalized, raycastDistance, obstacleLayer))
+            return false;
+
+        Debug.DrawLine(eyePosition, enemyTarget, Color.green);
+        return true;
+    }
+
+    void SetEnemyVisibility(Collider enemy, bool visible)
+    {
+        Renderer renderer = enemy.GetComponent<Renderer>();
+        SneakyEnemy sneaky = enemy.GetComponent<SneakyEnemy>();
+
+        if (renderer != null)
+            renderer.enabled = visible;
+
+        if (sneaky != null)
+            sneaky.SetSeen(visible);
+    }
+    
+    
+    
 }
