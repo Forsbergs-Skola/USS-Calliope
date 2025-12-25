837b7e9c0590ca94077dc7815856c4f1ce8ba2ac
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-12-25 14:45:44 +0100

    DialogueCanvas

--- Assets/Brad/Scripts/Dialogue/DialogueController.cs
+++ Assets/Brad/Scripts/Dialogue/DialogueController.cs
@@ -1,21 +1,55 @@
 using UnityEngine;
+using Events;
 using System.Collections.Generic;
 
 public class DialogueController : Singleton<DialogueController>
 {
     [SerializeField] private List<DialogueConversationSO> conversations;
-    
+
+    private void Start()
+    {
+        EventRelay.Instance.UIEvents.DialogueConvoStartedEVent.OnEventTriggered += HandleConvoStartedEvent;
+        EventRelay.Instance.UIEvents.DialogueConvoFinishedEvent.OnEventTriggered += HandleConvoEndedEvent;
+        EventRelay.Instance.UIEvents.DialogueLineStartedEVent.OnEventTriggered += HandleLineStartedEvent;
+        EventRelay.Instance.UIEvents.DialogueLineFinishedEvent.OnEventTriggered += HandleLineFinishedEvent;
+    }
+    private void OnDestroy()
+    {
+        EventRelay.Instance.UIEvents.DialogueConvoStartedEVent.OnEventTriggered -= HandleConvoStartedEvent;
+        EventRelay.Instance.UIEvents.DialogueConvoFinishedEvent.OnEventTriggered -= HandleConvoEndedEvent;
+        EventRelay.Instance.UIEvents.DialogueLineStartedEVent.OnEventTriggered -= HandleLineStartedEvent;
+        EventRelay.Instance.UIEvents.DialogueLineFinishedEvent.OnEventTriggered -= HandleLineFinishedEvent;
+    }
+
 
     /////////////
     // Private //
     /////////////
-    
+
+    private void HandleConvoStartedEvent(string convoID)
+    {
+
+    }
+    private void HandleConvoEndedEvent(string convoID)
+    {
+        if (!UIController.Instance.GetIsCanvasUp(EnumCanvasUIName.DIALOGUE)) return;
+        UIController.Instance.RemoveCanvas(EnumCanvasUIName.DIALOGUE);
+
+    }
+    private void HandleLineStartedEvent(string lineID)
+    {
+
+    }
+    private void HandleLineFinishedEvent(string lineID)
+    {
+
+    }
 
 
     /////////
     // API //
     /////////
-    
+
     public void StartConvoWithID(string convoID)
     {
         DialogueConversationSO convo = conversations.Find(conv => conv.ConvoID == convoID);
@@ -30,5 +64,7 @@ public class DialogueController : Singleton<DialogueController>
 
         }
     }
+
+
 
 }
