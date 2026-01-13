ce773caeece8a2114e52eb36f7d7ae783ddb155e
danglenzig <99054411+danglenzig@users.noreply.github.com>
2026-01-13 10:54:05 +0100

    Test commit 3

--- Assets/Prefabs/Pickups/ConsumablePrefab.cs
+++ Assets/Prefabs/Pickups/ConsumablePrefab.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class ConsumablePrefab : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private string pickupName;
+    [Range(1,100)][SerializeField] private int qty = 1;
+    [SerializeField] private string worldID;
+    [SerializeField] private ConsumableItemSO itemSO;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!other.CompareTag("Player")) return;
+        Olle.Scripts.PlayerController pc = other.GetComponent<Olle.Scripts.PlayerController>();
+        pc.HandleConsumablePickup(worldID, itemSO.ItemID, qty);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
