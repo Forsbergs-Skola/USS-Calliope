6866f577b2db878ba9bde0a54c77d4845654e7fb
danglenzig <brad.neal@student.forsbergsskola.se>
2026-01-13 19:59:41 +0100

    Logo sound

--- Assets/Brad/Scripts/UI/UISoundCatalogSO.cs
+++ Assets/Brad/Scripts/UI/UISoundCatalogSO.cs
@@ -6,9 +6,11 @@ public class UISoundCatalogSO : ScriptableObject
     [SerializeField] private AudioClip hoverClip;
     [SerializeField] private AudioClip pressClip;
     [SerializeField] private AudioClip pauseScreenSound;
+    [SerializeField] private AudioClip logoSound;
 
     public AudioClip HoverClip { get => hoverClip; }
     public AudioClip PressClip { get => pressClip; }
     public AudioClip PauseScreenSound { get => pauseScreenSound; }
+    public AudioClip LogoSound { get => logoSound; }
 
 }
