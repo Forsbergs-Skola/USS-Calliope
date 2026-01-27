2be2f14dc2ce5661a8a13c51f3563373fecdfc11
danglenzig <brad.neal@student.forsbergsskola.se>
2026-01-27 11:49:04 +0100

    Added PEGI 18 badge

--- Assets/Brad/Scripts/LogoSplashCanvas.cs
+++ Assets/Brad/Scripts/LogoSplashCanvas.cs
@@ -11,8 +11,18 @@ public class LogoSplashCanvas : MonoBehaviour, ICanvasUI
     [SerializeField] private float staySeconds = 4f;
     [SerializeField] private float fadeDuration = 3f;
     [SerializeField] private Image frontPanel;
+    [SerializeField] private RawImage pegi18;
+    [SerializeField] private GameObject logo;
 
     private UISoundPlayer soundPlayer { get => UIController.Instance.UISoundPlayer; }
+
+    private int phase = 0;
+
+
+    private void Awake()
+    {
+        pegi18.gameObject.SetActive(false);
+    }
 
     private void Start()
     {
@@ -56,11 +66,43 @@ public class LogoSplashCanvas : MonoBehaviour, ICanvasUI
 
     }
 
+
+    private void ShowPegi18()
+    {
+        frontPanel.color = new Color(0f, 0f, 0f, 1f);
+        logo.SetActive(false);
+        pegi18.gameObject.SetActive(true);
+        Tween fadeTween = TweenService.GetFloatTween(gameObject, 1.0f, 0.0f, fadeDuration, EnumTweenEase.QUART, EnumTweenDirection.IN);
+        fadeTween.OnValueUpdated += (value) =>
+        {
+            Color newColor = new Color(0f, 0f, 0f, value.x);
+            frontPanel.color = newColor;
+        };
+        fadeTween.OnFinished += () =>
+        {
+            StartCoroutine(WaitThenShowMain());
+        };
+        fadeTween.StartTween();
+    }
+
+    private System.Collections.IEnumerator WaitThenShowMain()
+    {
+        yield return new WaitForSeconds(staySeconds);
+        EventRelay.Instance.UIEvents.LogoSplashFinishedEvent.TriggerEvent();
+    }
+
+
     System.Collections.IEnumerator WaitThenContinue()
     {
         yield return new WaitForSecondsRealtime(staySeconds);
-        EventRelay.Instance.UIEvents.LogoSplashFinishedEvent.TriggerEvent();
+        ShowPegi18();
+
+        //EventRelay.Instance.UIEvents.LogoSplashFinishedEvent.TriggerEvent();
     }
+
+    
+
+
     System.Collections.IEnumerator WaitThenPlaySound()
     {
         yield return new WaitForSecondsRealtime(0.5f);
